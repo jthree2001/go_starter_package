f88243b5ae4ed3e80f0d5e30898e12e95cc4d886
Michael Lawson <lawsmichael@gmail.com>
2019-11-24 15:54:21 +0900

    finished server side of go project

---
+++ server/src/app/mutations/mutations.go
@@ -0,0 +1,10 @@
+package mutationsimport (
+  "github.com/graphql-go/graphql"
+  fields "app/mutations/fields"
+)
+var RootMutation = graphql.NewObject(graphql.ObjectConfig{
+  Name: "RootMutation",
+  Fields: graphql.Fields{
+    "createNotTodo": fields.CreateNotTodo,
+  },
+})
